6cd6a27a9e0d60849fe322ab31ef4295fdc59f48
Adi Hershkovitz <adihershkovitzz@gmail.com>
2022-06-21 10:23:03 +0300

    added RateJoke tests

---
+++ src/features/Rating/components/RateJoke/RateJoke.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+import RateJoke from './RateJoke';
+
+jest.mock('react-native-vector-icons/FontAwesome', () => 'icon');
+
+const mockCloseFn = jest.fn();
+
+const renderComponent = (Component: any) => {
+  const props = {
+    close: mockCloseFn,
+  };
+  return render(<Component {...props} />);
+};
+
+describe('RateJoke', () => {
+  it('Should render close button', () => {
+    const {getByLabelText} = renderComponent(RateJoke);
+    getByLabelText('RateJoke-close-button');
+  });
+
+  it('should call close function when pressing close button', () => {
+    const {getByLabelText} = renderComponent(RateJoke);
+    const closeButton = getByLabelText('RateJoke-close-button');
+    fireEvent.press(closeButton);
+    expect(mockCloseFn).toBeCalled();
+  });
+
+  it('Should render title: "How Funny?"', () => {
+    const {getByText} = renderComponent(RateJoke);
+    getByText('How funny?');
+  });
+});
--- src/features/Rating/components/RateJoke/RateJoke.tsx
+++ src/features/Rating/components/RateJoke/RateJoke.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import StarDisplay from '../../../../components/StarDisplay';
+import { applyAppiumLabel } from '../../../../services/appiumService';
 import styles from './style';
 
 const RateJoke = ({close, handleStarPress, rating}) => (
   <View style={styles.container}>
-    <TouchableOpacity style={styles.closeModal} onPress={close}>
+    <TouchableOpacity
+      style={styles.closeModal}
+      onPress={close}
+      {...applyAppiumLabel('RateJoke-close-button', true)}>
       <Icon name="close" style={styles.close} />
     </TouchableOpacity>
     <Text style={styles.modalText}>How funny?</Text>
